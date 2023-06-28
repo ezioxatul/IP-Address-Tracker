c687c5e92d646325efc4d1a0dc66494867880dc9
Atul <ezioxatul@gmail.com>
2023-06-28 21:57:56 +0530

    added domain name resolution
    Signed-off-by: Atul <ezioxatul@gmail.com>

--- main.js
+++ main.js
@@ -3,20 +3,22 @@ const userIp = document.querySelector(".user-ip")
 const userLocation = document.querySelector(".user-location")
 const userTimezone = document.querySelector(".user-timezone")
 const userIsp = document.querySelector(".user-isp")
+let input = document.getElementById("search-bar")
 
 
-function clear(){
-    document.getElementById("search-bar").value = "";
-}
+input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      submitBtn.click();
+    }
+  });
 
 
-
-// Event Listener
 submitBtn.addEventListener("click", function(){
-    let inputValue = document.getElementById("search-bar").value;
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_8qHA7uC9YWKMtF7OpCzcL6Cb7e3kC&ipAddress=${inputValue}`)
-.then(response => response.json())
-.then(data => {
+    let inputValue = input.value;
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_8qHA7uC9YWKMtF7OpCzcL6Cb7e3kC&ipAddress=${inputValue}&domain=${inputValue}`)
+    .then(response => response.json())
+    .then(data => {
 
     userIp.innerHTML = data.ip
     userLocation.innerHTML = data.location.city + ", " + data.location.country
@@ -24,6 +26,5 @@ submitBtn.addEventListener("click", function(){
     userIsp.innerHTML = data.isp
 })
 
-    clear();
 })
 
