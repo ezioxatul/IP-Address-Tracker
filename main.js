d8fefb187f84eba5f13e362588fb63aacbd2af38
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-06-28 23:51:02 -0600

    Added onload function to display user IP details as default
    Signed-off-by: Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>

--- main.js
+++ main.js
@@ -28,3 +28,15 @@ submitBtn.addEventListener("click", function(){
 
 })
 
+window.onload = function() {
+    let inputValue = input.value;
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_8qHA7uC9YWKMtF7OpCzcL6Cb7e3kC&ipAddress=${inputValue}`)
+    .then(response => response.json())
+    .then(data => {
+
+        userIp.innerHTML = data.ip;
+        userLocation.innerHTML = data.location.city + ", " + data.location.country;
+        userTimezone.innerHTML = "UTC " + data.location.timezone;
+        userIsp.innerHTML = data.isp;
+    })
+};
